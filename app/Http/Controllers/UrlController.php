62609b204a21913fa3285f40595d2cc4d951e7eb
iambasanta <sangpangbasanta@gmail.com>
2020-11-19 18:42:53 +0545

    Url shortner with all essentials components are created

---
+++ app/Http/Controllers/UrlController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UrlRequest;
+use App\Models\Url;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Str;
+
+class UrlController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Url::latest()->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function store(UrlRequest $request)
+    {
+        Url::create($request->all());
+        return response('Created', Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Url  $url
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Url $url)
+    {
+        return $url;
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Url  $url
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Url $url)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Url  $url
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Url $url)
+    {
+        $url->delete();
+        return response('', Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ app/Http/Requests/UrlRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UrlRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'original_url' => 'required|url',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'original_url.required' => 'Original Url field is Required.',
+            'original_url.url' => 'Original Url must be valid URL.',
+        ];
+    }
+}
